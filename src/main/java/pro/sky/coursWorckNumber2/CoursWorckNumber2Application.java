145157ecd206dccff4a467e0e07267d3b33a6667
pavel_gorpynyak <pavel_gorpynyak@bk.ru>
2022-10-07 14:49:17 +0300

    first commit

---
+++ src/main/java/pro/sky/coursWorckNumber2/CoursWorckNumber2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.coursWorckNumber2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursWorckNumber2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursWorckNumber2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/coursWorckNumber2/CoursWorckNumber2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.coursWorckNumber2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursWorckNumber2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
